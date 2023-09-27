ed2ca61850af22a8e3b9670ace86975757c2e926
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-28 00:33:52 +0200

    d

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <>
+  <div id="app">
+    <h1 class="main-title">Cendar</h1>
+    <h2 class="sub-title">by Megot</h2>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+/* Styles pour le titre */
+.main-title {
+    font-size: 3em; /* Taille du gros titre */
+    margin-bottom: 0; /* Enlever l'espacement en bas du titre */
+}
+
+.sub-title {
+    font-size: 1.5em; /* Taille du sous-titre */
+    color: #777; /* Couleur du sous-titre */
+    margin-top: 0; /* Enlever l'espacement en haut du sous-titre */
+}
+</style>
