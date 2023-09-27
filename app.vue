6445de97ca9d23e9883e8e2b19f9c9a8ba2a172a
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-28 00:50:10 +0200

    d

--- app.vue
+++ app.vue
@@ -1,26 +1,59 @@
 <template>
   <div id="app">
-    <h1 class="main-title">Cendar</h1>
-    <h2 class="sub-title">by Megot</h2>
+    <h1>L'Art de la Cendre</h1>
+    <h2>Célébrons la beauté des cendriers, la cendre et les mégots</h2>
+
+    <div class="cendrier">
+        <div class="megot"></div>
+    </div>
+
+    <p>Découvrez la beauté intemporelle des cendriers, la danse éthérée de la cendre et l'empreinte mémorable des mégots.</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    // Si vous avez d'autres composants, ils seront listés ici.
+  }
 }
 </script>
 
-<style scoped>
-/* Styles pour le titre */
-.main-title {
-    font-size: 3em; /* Taille du gros titre */
-    margin-bottom: 0; /* Enlever l'espacement en bas du titre */
+<style>
+body {
+    font-family: 'Press Start 2P', cursive; /* Une police pixelisée */
+    background-color: black;
+    color: white;
+    text-align: center;
+    padding: 50px;
 }
 
-.sub-title {
-    font-size: 1.5em; /* Taille du sous-titre */
-    color: #777; /* Couleur du sous-titre */
-    margin-top: 0; /* Enlever l'espacement en haut du sous-titre */
+h1, h2 {
+    color: orange;
+}
+
+.cendrier {
+    width: 100px;
+    height: 50px;
+    background-color: grey;
+    margin: 50px auto;
+    position: relative;
+}
+
+.megot {
+    width: 10px;
+    height: 30px;
+    background-color: white;
+    position: absolute;
+    bottom: 10px;
+    left: 45px;
+    animation: tomber 2s infinite;
+}
+
+@keyframes tomber {
+    0% { top: -30px; }
+    100% { top: 10px; }
 }
 </style>
+
