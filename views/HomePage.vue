c4db833880413a1fa892d9bed83abc65f230920c
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-28 16:01:44 +0200

    home

---
+++ views/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="history-container">
+      <h1>L'histoire des mégots de cigarette</h1>
+      <p>
+        Les mégots de cigarette, ces petits résidus que l'on trouve souvent éparpillés sur les trottoirs et dans les parcs, ont une histoire longue et complexe. Bien que la cigarette elle-même ait été popularisée au 20ème siècle, l'acte de fumer du tabac remonte à des milliers d'années, à des civilisations anciennes en Amérique du Sud et en Amérique centrale.
+  
+        Avec l'industrialisation et la production de masse au 20ème siècle, la cigarette est devenue un produit de consommation courant. Les publicités, les films et les célébrités ont contribué à glamouriser l'acte de fumer, et bientôt, les rues des villes du monde entier étaient jonchées de mégots.
+  
+        Les mégots de cigarette sont composés de filtres, généralement faits d'acétate de cellulose, et de restes de tabac. Ces filtres ont été introduits dans les années 1950 dans le but de rendre les cigarettes "plus sûres" en réduisant la quantité de goudron et de nicotine inhalée. Cependant, ils ont également contribué à un nouveau problème environnemental.
+  
+        Les mégots jetés négligemment mettent des années à se décomposer et libèrent des toxines dans l'environnement. Ils sont devenus l'un des déchets les plus couramment ramassés lors des nettoyages de plages et de rivières. De plus, les animaux, attirés par le reste de tabac, peuvent les ingérer, ce qui peut être fatal.
+  
+        Aujourd'hui, de nombreuses initiatives cherchent à sensibiliser le public à l'impact environnemental des mégots de cigarette et à encourager des méthodes de disposition plus responsables. Des poubelles spéciales pour les mégots sont installées dans de nombreuses villes, et des campagnes de sensibilisation sont menées pour éduquer le public.
+  
+        En conclusion, les mégots de cigarette, bien qu'ils soient petits, ont laissé une empreinte significative sur notre histoire et notre environnement. Il est essentiel de reconnaître leur impact et de prendre des mesures pour réduire leur empreinte.
+      </p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "HistoryOfCigaretteButts"
+  };
+  </script>
+  
+  <style scoped>
+  .history-container {
+    padding: 20px;
+    font-family: Arial, sans-serif;
+  }
+  
+  h1 {
+    font-size: 24px;
+    margin-bottom: 20px;
+  }
+  
+  p {
+    font-size: 16px;
+    line-height: 1.5;
+  }
+  </style>
+  
