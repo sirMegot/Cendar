5cba4bdc01cfa9158e73c4801bb4cae23e2af6ff
sirMegot <131977480+sirMegot@users.noreply.github.com>
2023-09-28 16:41:35 +0200

    redep

---
+++ router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from './app.vue';
+import HomePage from './views/HomePage.vue';
+import AboutPage from './views/AboutPage.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'hash',
+  routes: [
+    {
+      path: '/',
+      name: 'app',
+      component: App
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutPage
+    }
+    // Ajoutez d'autres routes ici
+  ]
+});
